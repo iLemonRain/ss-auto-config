c73a965d8fc84df41333207c73fc8654c76ed75b
Slim Wang <netbot@vip.qq.com>
2019-05-24 14:47:20 +0800

    完成图片地址抓取

---
+++ spider.py
@@ -0,0 +1,9 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+html = urllib.request.urlopen(
+    "https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"  # noqa: E501
+)
+
+soup = BeautifulSoup(html, 'html.parser')
+img_src = soup.select_one("#wiki-body > div > p > img")['src']
